00a224f15728b1b8f7b1f73615126b5f75fedce2
ra - our lady <rawalvarez731@gmail.com>
2024-06-07 02:33:25 -0500

    yo

---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import styled from "styled-components";
+
+const Header = styled.section`
+    width: 100%;
+    text-align: left;
+
+    a {
+        text-decoration: none;
+        color: black;
+        h1 { 
+            margin: 20px;
+        }
+    }
+`;
+
+export default Header;
---
+++ src/lib/serverActions.tsx
@@ -0,0 +1,12 @@
+'use server';
+
+// TODO: Implement
+export async function verifyNewEmail(email: string) {
+    return true;
+}
+
+// TODO: Implement
+export async function submitUserData(data: string) {
+    console.log(data);
+    return true;
+}
