8c039918ad7d146a6b880cbb11cd9afc3d7c58a5
ra - our lady <rawalvarez731@gmail.com>
2024-06-12 07:20:14 -0500

    added mroe styling; more pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,7 @@ import '@/styles/global.css'; // Import the global styles from the appropriate f
 export const metadata: Metadata = {
 	title: "GREasy",
 	description: "Personalized GRE flashcards directly to your inbox.",
+	metadataBase: new URL("https://greasyvocab.com"),
 	icons: [
 		{ rel: "icon", href: "/favicon.ico", url: "/favicon.ico" },
 	]
