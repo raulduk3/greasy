ac7de8a62fba7ffd9e218fbcfafc03faed09dcf5
ra - our lady <rawalvarez731@gmail.com>
2024-06-14 18:47:33 -0500

    mobile first tweaks, will continue to work on front end; v0.1.3

--- src/styles/layout/Body.tsx
+++ src/styles/layout/Body.tsx
@@ -9,7 +9,7 @@ const Body = styled.section`
     justify-content: space-evenly;
     gap: 2vh;
     flex: 1 1 auto;
-    min-height: calc(100vh - 19vh);
+    min-height: calc(100vh - 15vh);
     max-width: 100vw;
 `;
 
