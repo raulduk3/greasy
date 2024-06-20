437b34d7afb4b550475e4c6c67a9f99cefb525f9
ra - our lady <rawalvarez731@gmail.com>
2024-06-20 02:23:25 -0500

    working; love it with emails;

--- src/app/api/sendEmail/route.tsx
+++ src/app/api/sendEmail/route.tsx
@@ -1,5 +1,5 @@
 import { Resend } from 'resend';
-import { EmailTemplate } from '@/lib/EmailTemplate';
+import { EmailTemplate } from '@/lib/emailTemplate';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
