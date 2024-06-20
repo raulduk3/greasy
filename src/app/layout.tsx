67ab2a1de93b7d9aeaa91429332ef8bb70590002
ra - our lady <rawalvarez731@gmail.com>
2024-06-20 05:45:20 -0500

    added metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -122,14 +122,12 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en" className={roboto.className}>
-			<Head>
-				<script type="application/ld+json" dangerouslySetInnerHTML={{__html: JSON.stringify}}>
-				</script>
-			</Head>
 			<StyledComponentsRegistry>
 				<ThemeClient>
 					<Themer>
 						{/* For SEO */}
+						<script type="application/ld+json" dangerouslySetInnerHTML={{__html: JSON.stringify}}>
+						</script>
 						<h1 style={{
 							display: 'none',
 						}}>GREasy</h1>
