f7b9c9666419a7e20801dcef088fa66db33db154
ra - our lady <rawalvarez731@gmail.com>
2024-06-20 05:50:31 -0500

    added metadata

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -14,9 +14,9 @@ const AboutPage: React.FC = () => {
         }}>
             <h1>About Us</h1>
             <p style={{ maxWidth: '800px', textAlign: 'left' }}>
-            Welcome to GREasy! I’m <Link href="https://raulduke.com">Richard Álvarez</Link>, a recent grad who created this tool while prepping for the GRE. <br /> <br /> I wanted a study aid that was more engaging and effective than the usual, so I built GREasy to make mastering GRE vocabulary both fun and personalized. Dive in and enjoy the ride!            </p>
+            Welcome to GREasy! <br /> <br /> I’m <Link href="https://raulduke.com">Richard Álvarez</Link>, a recent grad who created this tool while prepping for the GRE. <br /> <br /> I wanted a study aid that was more engaging and effective than the usual, so I built GREasy to make mastering GRE vocabulary both fun and personalized. Dive in and enjoy the ride!            </p>
             <p style={{ maxWidth: '800px', textAlign: 'left' }}>
-                This project is not only about education; it's also about giving back. A portion of our proceeds is donated to support humanitarian efforts in Palestine. We believe in the power of education and the importance of helping those in need.
+                This project is not only about education; it's also about giving back. Fifty percent of proceeds are donated to support humanitarian efforts in Palestine. We believe in the power of education and the importance of helping those in need.
             </p>
             <p style={{ maxWidth: '800px', textAlign: 'left' }}>
                 Thank you for joining us on this journey. We hope that GREasy helps you achieve your GRE goals and makes your study sessions a little brighter. Together, let's make learning fun and impactful.
