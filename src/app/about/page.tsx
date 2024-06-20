3642b6eaa34d6925ec6d9c378c8ceb4f702c6183
ra - our lady <rawalvarez731@gmail.com>
2024-06-20 03:15:35 -0500

    added some pages

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -13,12 +13,12 @@ const AboutPage: React.FC = () => {
 			gap: '1rem',
         }}>
             <h1>About Us</h1>
-            <p style={{ maxWidth: '800px', textAlign: 'justify' }}>
+            <p style={{ maxWidth: '800px', textAlign: 'left' }}>
             Welcome to GREasy! I’m <Link href="https://raulduke.com">Richard Álvarez</Link>, a recent grad who created this tool while prepping for the GRE. <br /> <br /> I wanted a study aid that was more engaging and effective than the usual, so I built GREasy to make mastering GRE vocabulary both fun and personalized. Dive in and enjoy the ride!            </p>
-            <p style={{ maxWidth: '800px', textAlign: 'justify' }}>
+            <p style={{ maxWidth: '800px', textAlign: 'left' }}>
                 This project is not only about education; it's also about giving back. A portion of our proceeds is donated to support humanitarian efforts in Palestine. We believe in the power of education and the importance of helping those in need.
             </p>
-            <p style={{ maxWidth: '800px', textAlign: 'justify' }}>
+            <p style={{ maxWidth: '800px', textAlign: 'left' }}>
                 Thank you for joining us on this journey. We hope that GREasy helps you achieve your GRE goals and makes your study sessions a little brighter. Together, let's make learning fun and impactful.
             </p>
         </div>
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -12,10 +12,10 @@ const ContactPage: React.FC = () => {
 			gap: '1rem',
         }}>
             <h1>Contact Us</h1>
-            <p style={{ maxWidth: '800px', textAlign: 'justify' }}>
+            <p style={{ maxWidth: '800px', textAlign: 'left' }}>
                 We would love to hear from you! Whether you have questions, feedback, or just want to connect, feel free to reach out.
             </p>
-            <p style={{ maxWidth: '800px', textAlign: 'justify' }}>
+            <p style={{ maxWidth: '800px', textAlign: 'left' }}>
                 <strong>Email:</strong> You can email us at <a href="mailto:support@greasyvocab.com" style={{ color: '#7DDF64', textDecoration: 'none' }}>support@greasyvocab.com</a>. <br /> <br /> We strive to respond to all inquiries within 24 hours.
                 We look forward to connecting with you and assisting you in your GRE preparation journey!
             </p>
