19c5c7a133e1e2ec3abc3b648359e796994c6188
ra - our lady <rawalvarez731@gmail.com>
2024-06-21 12:01:59 -0500

    switched to tailwind

--- src/components/PricingSection.tsx
+++ src/components/PricingSection.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 
 export const PricingSection: React.FC = () => {
     return (
-        <div className="flex flex-col items-center justify-center p-6 md:flex-row md:justify-center gap-2 md:gap-4">
+        <div className="flex flex-col justify-center p-6 md:flex-row md:justify-center gap-2 md:gap-4">
             {packages.map((pkg, index) => (
                 <PackageCard key={index} {...pkg} />
             ))}
@@ -21,21 +21,25 @@ const PackageCard: React.FC<{
     link: string;
     linkText: string;
 }> = ({ title, price, description, features, link, linkText }) => (
-    <div className="bg-white text-gray-900 p-8 m-2 gap-1 shadow-lg rounded-sm w-11/12 md:max-w-auto text-left flex flex-col">
-        <h1 className="text-2xl">{title}</h1>
-        <p className="text-l mb-1">{price}<span className='italic text-sm text-gray-900'> | {description}</span></p>
-        <ul className="list-disc ml-4 text-gray-900">
-            {features.map((feature, idx) => (
-                <li key={idx}>{feature}</li>
-            ))}
-        </ul>
-        <Link href={link}
-                className="shadow bg-green-500 text-white py-2 px-4 rounded mt-4 text-center font-bold no-underline hover:bg-green-600 transition duration-200"
-                role="button"
-                aria-label={`Start ${title}`}
-        >
-                {linkText}
-        </Link>
+    <div className='flex flex-col items-center align-center basis-full flex-1'>
+        <div className="bg-white flex flex-1 h-full flex-col text-gray-900 p-8 m-2 gap-1 shadow-lg rounded-sm w-11/12 md:max-w-auto text-left flex flex-col">
+            <h1 className="text-xl">{title}</h1>
+            <p className="text-l mb-1">{price}<span className='italic text-sm text-gray-900'> | {description}</span></p>
+            <ul className="list-disc ml-4 text-gray-900">
+                {features.map((feature, idx) => (
+                    <li key={idx}>{feature}</li>
+                ))}
+            </ul>
+            <div className='flex flex-1 justify-items-end'> 
+                <Link href={link}
+                        className="shadow bg-green-500 text-white w-full py-2 px-4 rounded mt-4 self-end text-center font-bold no-underline hover:bg-green-600 transition duration-200"
+                        role="button"
+                        aria-label={`Start ${title}`}
+                        >
+                        {linkText}
+                </Link>
+            </div>
+        </div>
     </div>
 );
 
@@ -45,9 +49,10 @@ const packages = [
         price: "$2.00",
         description: "Same day delivery",
         features: [
+            "Complete cards with word, part of speech, definition, and sentence.",
+            "Increased visual and contextual customizability",
             "Personalized sentences using GPT-4",
             "Sent to your email as printable PDF",
-            "Increased customizability",
             "Flashcards viewable as webpage",
             "Latest GRE words",
         ],
@@ -59,9 +64,11 @@ const packages = [
         price: "$1.00",
         description: "Same day delivery",
         features: [
+            "Complete cards with word, part of speech, definition, and sentence.",
             "Personalized sentences using GPT-3.5",
             "Sent to your email as printable PDF",
             "Flashcards viewable as webpage",
+            "Latest GRE words",
         ],
         link: "/q/basic",
         linkText: "Start"
@@ -71,10 +78,9 @@ const packages = [
         price: "$0.00",
         description: "Same day delivery",
         features: [
-            "Generated using GPT-3.5",
-            "Complete cards with word, part of speech, definition, and sentence.",
-            "Limited vocabulary",
             "Sent to your email for viewing",
+            "Generated using GPT-3.5",
+            "Limited vocabulary",
         ],
         link: "/q/demo",
         linkText: "Try for free!"
