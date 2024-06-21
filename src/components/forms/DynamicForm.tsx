f038decf4d008ba5099bfc0aacd3ebf7bf225c53
ra - our lady <rawalvarez731@gmail.com>
2024-06-21 12:29:49 -0500

    switched to tailwind

--- src/components/forms/DynamicForm.tsx
+++ src/components/forms/DynamicForm.tsx
@@ -30,8 +30,8 @@ const DynamicForm: React.FC<DynamicFormProps> = ({ title, length, description, p
     };
 
     return (
-        <div className="flex flex-col items-center justify-center mx-32 my-8">
-            <form onSubmit={handleSubmit} className="flex flex-col items-center justify-center p-6 my-6 bg-white text-gray-900 shadow-md w-full max-w-md">
+        <div className="flex flex-col items-center justify-center my-10">
+            <form onSubmit={handleSubmit} className="flex flex-col items-center rounded justify-center p-6 bg-white text-gray-900 shadow-md max-w-lg">
                 <div className="w-full my-2">
                     <h2 className="text-2xl font-bold">{title}</h2>
                     <p className="text-lg">{description}</p>
