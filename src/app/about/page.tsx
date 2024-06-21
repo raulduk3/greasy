2fcc5b62261e78ea2c9a97133bc4eea356439fad
ra - our lady <rawalvarez731@gmail.com>
2024-06-21 11:17:43 -0500

    switched to tailwind

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -6,7 +6,10 @@ const AboutPage: React.FC = () => {
         <div className="flex flex-col m-6 p-5 font-sans gap-4 w-3/4 md:w-auto bg-white text-black rounded-lg">
             <h1 className="text-3xl font-bold">About GREasy</h1>
             <p className="max-w-xl text-left text-left">
-                Hi name is <Link href="https://raulduke.com" className="text-green-500 no-underline">Richard Álvarez</Link>. I am a recent college grad who created this tool while prepping for the GRE. I wanted a study aid that was more engaging and effective than the usual, so I built GREasy to make mastering GRE vocabulary both fun and personalized. 
+                Hi my name is <Link href="https://raulduke.com" className="text-green-500 no-underline">Richard Álvarez</Link> 
+            </p>
+            <p>
+                I am a recent college grad who created this tool while prepping for the GRE. I wanted a study aid that was more engaging and effective than the usual, so I built GREasy to make mastering GRE vocabulary both fun and personalized. 
             </p>
             <p className="max-w-xl text-left text-left">
                 This project is not only about education; it's also about giving back. Fifty percent of proceeds are donated to support humanitarian efforts in Palestine. I believe in the power of education and the importance of helping those in need.
