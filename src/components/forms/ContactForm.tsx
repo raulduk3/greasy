61b709128404fc4d47fcbc4d79533f12eee74ab9
ra - our lady <rawalvarez731@gmail.com>
2024-06-24 04:17:19 -0500

    more paypyal; closer to finances being done 😭

--- src/components/forms/ContactForm.tsx
+++ src/components/forms/ContactForm.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import React, { useState } from 'react';
+import Link from 'next/link';
 import { userVerify } from '@/lib/user/verify';
 
 function ContactForm({ onSubmit, reusable }: { onSubmit: (data: any) => void, reusable: boolean }): React.ReactElement {
@@ -31,7 +32,6 @@ function ContactForm({ onSubmit, reusable }: { onSubmit: (data: any) => void, re
             <form onSubmit={handleSubmit} className="flex flex-col items-center rounded justify-center p-8 bg-white text-gray-900 shadow-md max-w-lg">
                 <div className="w-full mb-2">
                     <h2 className="text-2xl mb-1">Begin by providing your basic contact information.</h2>
-                    <p className="text-lg">We will contact you via email once only.</p>
                     <p className="text-lg">Be sure to provide the <span className='underline font-bold'>correct</span> email address.</p>
                 </div>
                 <div className="w-full my-2 flex flex-col items-start">
@@ -46,10 +46,16 @@ function ContactForm({ onSubmit, reusable }: { onSubmit: (data: any) => void, re
                         <input name="email" type="email" placeholder='example@web.com' onChange={handleChange} required className="border-b-2 border-gray-300 focus:border-green-400 outline-none p-2" />
                     </label>
                 </div>
-                <div className="w-full my-2 flex flex-col items-start">
-                    <label className="w-full flex text-smitems-center gap-2">
+                <div className="w-full my-2 flex flex-col items-start align-center">
+                    <label className="w-full flex text-sm gap-2">
+                        Confirm email?
                         <input type='checkbox' name='toggle' value='toggle' required className="w-5 h-5 text-green-400"/>
-                        Confirm permission to email?
+                    </label>
+                </div>
+                <div className="w-full my-2 flex flex-col items-center align-center">
+                    <label className="w-full flex text-sm gap-2">
+                        <Link className="p-0 underline" href='/terms-of-service'>Agree to Terms of Serivce?</Link>
+                        <input type='checkbox' name='toggle' value='toggle' required className="w-5 h-5 text-green-400"/>
                     </label>
                 </div>
                 <div className="self-start w-5/12 mt-4">
