17d91e7f44571bad726408168404409d4691ac77
ra - our lady <rawalvarez731@gmail.com>
2024-06-25 18:01:33 -0500

    vault update

--- src/components/AdminDashboard.tsx
+++ src/components/AdminDashboard.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 
 export const dynamic = 'force-dynamic';
 
