9c164d0f9d0b51d0097d27136179f6dbfbe2db35
ra - our lady <rawalvarez731@gmail.com>
2024-06-25 07:41:56 -0500

    more paypyal; closer to finances being done 😭

--- src/app/api/orders/route.tsx
+++ src/app/api/orders/route.tsx
@@ -1,6 +1,8 @@
 import { sql } from '@vercel/postgres';
 import { verifyToken } from '@/lib/auth';
 
+export const dynamic = 'force-dynamic';
+
 export async function GET(req: Request) {
     const authHeader = req.headers.get('Authorization');
     if (!authHeader || !authHeader.startsWith('Bearer ')) {
