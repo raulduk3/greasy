d9f8e50b970c5f4b7d96b39f392bc04ea20fa0ad
ra - our lady <rawalvarez731@gmail.com>
2024-06-27 10:29:18 -0500

    whoops made a mistake; added more shitacki

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -110,7 +110,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 		<html lang="en" className={inter.className}>
 			<body className={cn("flex flex-col min-h-screen w-[100vw] overflow-x-hidden font-sans bg-slate-700 text-white", inter.className)}>
 				<Header></Header>
-				<main className="flex flex-col grow flex-1 items-center justify-center mt-[5rem] gap-6 lg:gap-8 justify-start w-full h-full">
+				<main className="flex flex-col grow flex-1 items-center justify-center mt-[5rem] gap-6 lg:gap-6 justify-start w-full h-full">
 					{children}
 					<Analytics />
 					<script key={'ldjson-script'} type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(JSONLD) }} />
